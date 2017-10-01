22e642a50297f707fbcf35cfe868ac72373e0e08
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-10-01 19:31:57 +0200

    Added Spring project.

---
+++ elasticsearch-data-updater/src/main/java/es/loganalyzer/elasticsearchdataupdater/elasticsearch_data_updater/App.java
@@ -0,0 +1,13 @@
+package es.loganalyzer.elasticsearchdataupdater.elasticsearch_data_updater;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
