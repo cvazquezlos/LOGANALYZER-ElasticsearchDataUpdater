fbccdf38fcfee90a086212ba6f458c0ac3dfc6fc
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-10-08 10:43:09 +0200

    Update project.

--- src/main/java/es/loganalyzer/elasticsearchdataupdater/app/Application.java
+++ src/main/java/es/loganalyzer/elasticsearchdataupdater/app/Application.java
@@ -19,53 +19,46 @@ import es.loganalyzer.elasticsearchdataupdater.repository.LogRepository;
 public class Application {
 	
 	@Autowired
-	public static LogRepository repository;
+	public LogRepository repository;
 
-	public static void main(String[] args) {
+	public void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		inserDataIntoElasticsearch();
 	}
 
-	public static void inserDataIntoElasticsearch() {
+	public void inserDataIntoElasticsearch() {
 		ArrayList<String> data = new ArrayList<String>();
 		try {
 			File f = new File("log.txt");
 			BufferedReader br = new BufferedReader(new FileReader(f));
 			String line = "";
 			while ((line = br.readLine()) != null) {
-				System.out.println(line);
 				data.add(line);
 			}
 			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("---------------------------------------------------------------------------------------------------------------- start01------------------------------------");
 		Integer i = 0;
 		while (data.get(0).indexOf("2") != 0) {
 			Log log = new Log(i.toString(), data.get(0));
-			System.out.println(data.get(0));
 			data.remove(0);
 			i++;
 		}
-		System.out.println("---------------------------------------------------------------------------------------------------------------- start02------------------------------------");
 		while (data.get(0).indexOf("T") != 0) {
 			String[] args = getArgs(data.get(0));
 			Log log = new Log(i.toString(), data.get(0), args[0], args[1], args[2], args[3], args[4]);
-			System.out.println(data.get(0));
 			data.remove(0);
 			i++;
 		}
-		System.out.println("---------------------------------------------------------------------------------------------------------------- start03------------------------------------");
-		while (data.get(0) != null) {
+		while (!data.isEmpty()) {
 			Log log = new Log(i.toString(), data.get(0));
-			System.out.println(data.get(0));
 			data.remove(0);
 			i++;
 		}
 	}
 
-	private static String[] getArgs(String string) {
+	private String[] getArgs(String string) {
 		String[] args = new String[5];
 		String[] data = string.split(" ");
 		args[0] = data[0] + " " + data[1];
@@ -74,7 +67,7 @@ public class Application {
 		args[3] = data[5];
 		args[4] = data[7];
 		for (int i = 8; i < data.length; i++) {
-			args[4] += args[4] + " " + data[i];
+			args[4] += " " + data[i];
 		}
 		return args;
 	}
