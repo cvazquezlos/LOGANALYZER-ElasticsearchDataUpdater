23691e1af9a80e3d75ffad4674f657da37892ab9
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-10-08 15:18:11 +0200

    Added custom test number.

--- src/main/java/es/loganalyzer/elasticsearchdataupdater/app/Application.java
+++ src/main/java/es/loganalyzer/elasticsearchdataupdater/app/Application.java
@@ -1,8 +1,10 @@
 package es.loganalyzer.elasticsearchdataupdater.app;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -12,7 +14,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 import es.loganalyzer.elasticsearchdataupdater.model.Log;
-import es.loganalyzer.elasticsearchdataupdater.repository.LogRepository;
 import es.loganalyzer.elasticsearchdataupdater.service.ESLogService;
 
 @SpringBootApplication
@@ -33,6 +34,24 @@ public class Application {
 
 	public static void inserDataIntoElasticsearch() {
 		ArrayList<String> data = new ArrayList<String>();
+		Integer testNo = 0;
+		try {
+			File f = new File("testno.txt");
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				testNo = Integer.valueOf(line);
+			}
+			data.add(0, "[INFO] Building project and starting unit test number " + testNo + "...");
+			testNo+=1;
+			BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+			bw.write(testNo.toString());
+			br.close();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		testNo-=1;
 		try {
 			File f = new File("log.txt");
 			BufferedReader br = new BufferedReader(new FileReader(f));
@@ -44,23 +63,24 @@ public class Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		data.add("[INFO] Finishing unit test number " + testNo + "...");
 		Integer i = 0;
 		while (data.get(0).indexOf("2") != 0) {
+			System.out.println(data.get(0));
 			Log log = new Log(i.toString(), data.get(0));
-			service.save(log);
 			data.remove(0);
 			i++;
 		}
 		while (data.get(0).indexOf("T") != 0) {
 			String[] args = getArgs(data.get(0));
+			System.out.println(data.get(0));
 			Log log = new Log(i.toString(), data.get(0), args[0], args[1], args[2], args[3], args[4]);
-			service.save(log);
 			data.remove(0);
 			i++;
 		}
 		while (!data.isEmpty()) {
+			System.out.println(data.get(0));
 			Log log = new Log(i.toString(), data.get(0));
-			service.save(log);
 			data.remove(0);
 			i++;
 		}
